4f0018689c64a1303c0fcdeadea5e60d674eaf8f
PraiseImmanuel <praiseimmanuel601@gmail.com>
2022-03-29 00:27:24 +0100

    Complete another project for the break

---
+++ README-template.md
@@ -0,0 +1,52 @@
+# Frontend Mentor - E-commerce product page solution
+
+This is a solution to the [E-commerce product page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/ecommerce-product-page-UPsZ9MJp6). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [Continued development](#continued-development)
+- [Author](#author)
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Open a lightbox gallery by clicking on the large product image
+- Switch the large product image by clicking on the small thumbnail images
+- Add items to the cart
+- View the cart and remove items from it
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## Features
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+
+
+### Continued development
+Under taking this projet made me realised i had so much more to cover in Javascript. So i took up FreeCodeCamp tutorial for this purpose.
+
+
+## Author
+
+- Website - [Add your name here](https://www.your-site.com)
+- Frontend Mentor - [@yourusername](https://www.frontendmentor.io/profile/yourusername)
+- Twitter - [@yourusername](https://www.twitter.com/yourusername)
---
+++ index.js
@@ -0,0 +1,313 @@
+// Isolation mode for product
+for(let i = 0; i <= 3; i++){
+    document.getElementsByClassName("product")[i].addEventListener("dblclick", function(){
+        document.querySelector(".when-clicked").style.display = "flex";
+    });
+}
+
+// Display None for smaller devices
+if(window.screen.width <= "1152px"){
+    document.querySelector(".when-clicked").style.display = "none";
+}
+
+//Menu
+function menu(){
+    let sideNav = document.getElementById("side-nav");
+    sideNav.style.display = "flex";
+}
+
+//Close
+function closed(){
+    document.getElementById("side-nav").style.display = "none";
+}
+
+function closeIsolation(){
+    document.querySelector(".when-clicked").style.display = "none";
+}
+
+// Hover over cart image
+const navCartImg = document.getElementById("nav-cart-img");
+
+navCartImg.addEventListener("mouseover", function (){
+    navCartImg.setAttribute("src", "./images/icon-cart -black.svg");
+})
+
+navCartImg.addEventListener("mouseout", function (){
+    navCartImg.setAttribute("src", "./images/icon-cart.svg");
+})
+
+// Display Cart Box
+function clickNavCartImg(){
+    const cart = document.getElementById("cart");
+    const showNumItems = document.getElementById("show-num-items");
+    if (cart.style.display == "flex"){
+        cart.style.display = "none";
+        showNumItems.style.display = "none";
+    }
+
+    else{
+        cart.style.display = "flex";
+        showNumItems.style.display = "inline-block";
+    }
+}
+
+// Hide product information on cart
+for(let i = 0; i <= 3; i++){
+    document.getElementsByClassName("product-info")[i].style.display = "none";
+}
+
+//Display product information on cart
+let firstItemClosed = document.getElementsByClassName("product-info")[0].style.display;
+let secondItemClosed = document.getElementsByClassName("product-info")[1].style.display;
+let thirdItemClosed = document.getElementsByClassName("product-info")[2].style.display;
+let fourthItemClosed = document.getElementsByClassName("product-info")[3].style.display;
+
+if(firstItemClosed == 'none'  &&  secondItemClosed == 'none' && thirdItemClosed == 'none' && fourthItemClosed == 'none'){
+    document.querySelector(".empty-cart").style.display = "block";
+    document.querySelector(".checkout").style.display = "none";
+}
+
+//  PRODUCT SLIDES  //
+
+// Chose a new product by clicking thumbnail
+function click(a){
+    for(i = 0; i < 4; i++){
+        document.getElementsByClassName("product-thumbnail")[i].classList.remove("border");
+        document.getElementsByClassName("w-product-thumbnail")[i].classList.remove("border");
+        
+        document.getElementsByClassName("img-prod-thumbnail")[i].classList.remove("opacity");
+        document.getElementsByClassName("w-img-prod-thumbnail")[i].classList.remove("opacity");
+    }
+
+    document.getElementsByClassName("product-thumbnail")[a - 1].classList.add("border");
+    document.getElementsByClassName("w-product-thumbnail")[a - 1].classList.add("border");
+
+    document.getElementsByClassName("img-prod-thumbnail")[a - 1].classList.add("opacity");
+    document.getElementsByClassName("w-img-prod-thumbnail")[a - 1].classList.add("opacity");
+
+    showProducts(productIndex= a );
+    showProductsWhenCliked(productIndex = a);
+}
+
+let productIndex = 1;
+showProducts(productIndex);
+showProductsWhenCliked(productIndex)
+
+// Controls
+function plusSlides(n) {
+    showProducts(productIndex += n);
+}
+
+function plusSlidesWhenClicked(n){
+    showProductsWhenCliked(productIndex += n);
+}
+
+function showProducts(n) {
+    let i;
+    let slides = document.getElementsByClassName("product");
+    if (n > slides.length) {productIndex = 1}
+    if (n < 1) {productIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }    
+    slides[productIndex - 1].style.display = "block";
+    preciseItemsNumAndPrice();
+    whenEmpty();
+}
+
+function showProductsWhenCliked(n) {
+    let i;
+    let slidesWhenC = document.getElementsByClassName("w-product");
+    if (n > slidesWhenC.length) {productIndex = 1}
+    if (n < 1) {productIndex = slidesWhenC.length}
+    for (i = 0; i < slidesWhenC.length; i++) {
+        slidesWhenC[i].style.display = "none";
+    }    
+    slidesWhenC[productIndex - 1].style.display = "block";
+}
+
+//Delete items from cart
+
+function delete_btn(c){
+    document.getElementsByClassName("product-info")[c -1].style.display = "none";
+    document.getElementsByClassName("num")[c].innerHTML = 1;
+    hideCheckout();
+    whenEmpty();
+}
+
+//Hide checkout and Display Cart's empty
+
+function hideCheckout(){
+    let firstItemClosed = document.getElementsByClassName("product-info")[0].style.display;
+    let secondItemClosed = document.getElementsByClassName("product-info")[1].style.display;
+    let thirdItemClosed = document.getElementsByClassName("product-info")[2].style.display;
+    let fourthItemClosed = document.getElementsByClassName("product-info")[3].style.display;
+    
+    if(firstItemClosed == 'none'  &&  secondItemClosed == 'none' && thirdItemClosed == 'none' && fourthItemClosed == 'none'){
+        document.querySelector(".empty-cart").style.display = "block";
+        document.querySelector(".checkout").style.display = "none";
+    }
+
+}
+
+//Checkout of cart
+function checkout(){
+    document.getElementById("cart").style.display = "none";
+}
+
+//Click add to cart
+function addToCart(){ 
+    let firstProduct = document.getElementsByClassName("product")[0].style.display;
+    let secondProduct = document.getElementsByClassName("product")[1].style.display;
+    let thirdProduct = document.getElementsByClassName("product")[2].style.display;
+    let fourthProduct = document.getElementsByClassName("product")[3].style.display;
+
+    if(firstProduct == "block"){
+        document.getElementsByClassName("product-info")[0].style.display = "flex";
+    }
+
+    if(secondProduct == "block"){
+        document.getElementsByClassName("product-info")[1].style.display = "flex";
+    }
+
+    if(thirdProduct == "block"){
+        document.getElementsByClassName("product-info")[2].style.display = "flex";
+    }
+
+    if(fourthProduct == "block"){
+        document.getElementsByClassName("product-info")[3].style.display = "flex";
+    }
+
+    let firstItemClosed = document.getElementsByClassName("product-info")[0].style.display;
+    let secondItemClosed = document.getElementsByClassName("product-info")[1].style.display;
+    let thirdItemClosed = document.getElementsByClassName("product-info")[2].style.display;
+    let fourthItemClosed = document.getElementsByClassName("product-info")[3].style.display;
+    
+    if (firstItemClosed == 'flex' || secondItemClosed == 'flex' || thirdItemClosed == 'flex' || fourthItemClosed == 'flex'){
+        document.querySelector(".empty-cart").style.display = "none";
+        document.querySelector(".checkout").style.display = "block";
+    }   
+
+    document.getElementById("cart").style.display = "flex";
+    document.getElementById("show-num-items").style.display = "block";
+
+    preciseItemsNumAndPrice();
+    
+}
+
+//Click minus button
+function minus(){
+    let firstProduct = document.getElementsByClassName("product")[0].style.display;
+    let secondProduct = document.getElementsByClassName("product")[1].style.display;
+    let thirdProduct = document.getElementsByClassName("product")[2].style.display;
+    let fourthProduct = document.getElementsByClassName("product")[3].style.display;
+
+    if(firstProduct == "block"){
+        document.getElementsByClassName("num")[1].innerHTML--;
+    }
+
+    if(secondProduct == "block"){
+        document.getElementsByClassName("num")[2].innerHTML--;
+    }
+
+    if(thirdProduct == "block"){
+        document.getElementsByClassName("num")[3].innerHTML--;
+    }
+
+    if(fourthProduct == "block"){
+        document.getElementsByClassName("num")[4].innerHTML--;
+    }
+
+    max_minNumItems();
+    preciseItemsNumAndPrice();
+    hideCheckout();
+}
+
+//Click plus button
+function plus(){
+    let firstProduct = document.getElementsByClassName("product")[0].style.display;
+    let secondProduct = document.getElementsByClassName("product")[1].style.display;
+    let thirdProduct = document.getElementsByClassName("product")[2].style.display;
+    let fourthProduct = document.getElementsByClassName("product")[3].style.display;
+
+    if(firstProduct == "block"){
+        document.getElementsByClassName("num")[1].innerHTML++;
+    }
+
+    if(secondProduct == "block"){
+        document.getElementsByClassName("num")[2].innerHTML++;
+    }
+
+    if(thirdProduct == "block"){
+        document.getElementsByClassName("num")[3].innerHTML++;
+    }
+
+    if(fourthProduct == "block"){
+        document.getElementsByClassName("num")[4].innerHTML++;
+    }
+
+    max_minNumItems();
+    preciseItemsNumAndPrice();
+}
+
+function whenEmpty(){
+    let firstItemClosed = document.getElementsByClassName("product-info")[0].style.display;
+    let secondItemClosed = document.getElementsByClassName("product-info")[1].style.display;
+    let thirdItemClosed = document.getElementsByClassName("product-info")[2].style.display;
+    let fourthItemClosed = document.getElementsByClassName("product-info")[3].style.display;
+    
+    if (firstItemClosed != 'flex' && secondItemClosed != 'flex' && thirdItemClosed != 'flex' && fourthItemClosed != 'flex'){
+        document.getElementsByClassName("num")[0].innerHTML = 0;
+    }   
+}
+
+function preciseItemsNumAndPrice(){
+    let firstProduct = document.getElementsByClassName("product")[0].style.display;
+    let secondProduct = document.getElementsByClassName("product")[1].style.display;
+    let thirdProduct = document.getElementsByClassName("product")[2].style.display;
+    let fourthProduct = document.getElementsByClassName("product")[3].style.display;
+
+    if(firstProduct == "block"){
+        document.getElementsByClassName("num")[0].innerHTML = document.getElementsByClassName("num")[1].innerHTML;
+        document.querySelector("div.one").innerHTML = document.getElementsByClassName("num")[1].innerHTML;
+        document.getElementsByClassName("price-total")[0].innerHTML = "$" + 
+        (document.getElementsByClassName("num")[1].innerHTML * 125) + ".00";
+    }
+
+    if(secondProduct == "block"){
+        document.getElementsByClassName("num")[0].innerHTML = document.getElementsByClassName("num")[2].innerHTML;
+        document.querySelector("div.one").innerHTML = document.getElementsByClassName("num")[2].innerHTML;
+        document.getElementsByClassName("price-total")[1].innerHTML = "$" + 
+        (document.getElementsByClassName("num")[2].innerHTML * 125) + ".00";
+    }
+
+    if(thirdProduct == "block"){
+        document.getElementsByClassName("num")[0].innerHTML = document.getElementsByClassName("num")[3].innerHTML;
+        document.querySelector("div.one").innerHTML = document.getElementsByClassName("num")[3].innerHTML;
+        document.getElementsByClassName("price-total")[2].innerHTML = "$" + 
+        (document.getElementsByClassName("num")[3].innerHTML * 125) + ".00";
+
+    }
+
+    if(fourthProduct == "block"){
+        document.getElementsByClassName("num")[0].innerHTML = document.getElementsByClassName("num")[4].innerHTML;
+        document.querySelector("div.one").innerHTML = document.getElementsByClassName("num")[4].innerHTML;
+        document.getElementsByClassName("price-total")[3].innerHTML = "$" + 
+        (document.getElementsByClassName("num")[4].innerHTML * 125) + ".00";
+    }
+
+}
+
+function max_minNumItems(){
+    for(let i = 1; i <= 4; i++){
+        if(document.getElementsByClassName("num")[i].innerHTML >=5 ){
+            document.getElementsByClassName("num")[i].innerHTML = 5;
+            document.querySelector("div.one").innerHTML = 5;
+        }
+
+        if(document.getElementsByClassName("num")[i].innerHTML <= 0 ){
+            document.getElementsByClassName("num")[i].innerHTML = 1;
+        }
+    }
+
+}
